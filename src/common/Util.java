e619fbb30653e18a4c4567d8cbeb1ffc23a3f5b6
Jeong Geumdam <61661198+jgd0423@users.noreply.github.com>
2021-02-03 09:54:57 +0900

    first commit

---
+++ src/common/Util.java
@@ -0,0 +1,108 @@
+package common;
+
+import java.util.Calendar;
+
+public class Util {
+	public int[] getDateTime() {
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int min = cal.get(Calendar.MINUTE);
+		int sec = cal.get(Calendar.SECOND);
+		
+		int[] result = new int[6];
+		result[0] = year;
+		result[1] = month;
+		result[2] = day;
+		result[3] = hour;
+		result[4] = min;
+		result[5] = sec;
+		return result;
+	}
+
+	public int numberCheck(String pageNumStr, int defaultNumber) {
+		String defaultPageNumber = String.valueOf(defaultNumber);
+		
+		if (pageNumStr == null || pageNumStr.trim().equals("")) {
+			pageNumStr = defaultPageNumber;
+		}
+		
+		try {
+			Double.parseDouble(pageNumStr);
+			return Integer.parseInt(pageNumStr);
+		} catch (Exception e) {
+			return Integer.parseInt(defaultPageNumber);
+		}
+	}
+
+	public String list_gubunCheck(String list_gubun) {
+		if (list_gubun == null || list_gubun.trim().equals("")) {
+			list_gubun = "all";
+		}
+		
+		list_gubun = list_gubun.trim();
+		if (list_gubun.equals("all")) {} 
+		else if (list_gubun.equals("ing")) {} 
+		else if (list_gubun.equals("end")) {} 
+		else { list_gubun = "all"; }
+		
+		return list_gubun;
+	}
+
+	public String[] searchCheck(String search_option, String search_data, String search_date_start, String search_date_end, String search_date_check) {
+		String[] result = new String[5];
+		
+		if (search_option == null || search_option.trim().equals("")) {
+			search_option = "";
+		}
+		search_option = search_option.trim();
+		
+		if (search_option.equals("")) {}
+		else if (search_option.equals("question")) {}
+		else { search_option = ""; }
+		
+		if (search_data == null || search_data.trim().equals("")) {
+			search_data = "";
+		}
+		search_data = search_data.trim();
+		
+		if (search_option.equals("") || search_data.equals("")) {
+			search_option = "";
+			search_data = "";
+		}
+		
+		if (search_date_check == null || search_date_check.trim().equals("")) {
+			search_date_check = "";
+		}
+		
+		if (search_date_check.equals("O")) {
+			if (search_date_start == null || search_date_start.trim().equals("")) {
+				search_date_start = "";
+			}
+			search_date_start = search_date_start.trim();
+			
+			if (search_date_end == null || search_date_end.trim().equals("")) {
+				search_date_end = "";
+			}
+			search_date_end = search_date_end.trim();
+			
+			if (search_date_start.equals("") || search_date_end.equals("")) {
+				search_date_check = "";
+			}
+		} else {
+			search_date_start = "";
+			search_date_end = "";
+			search_date_check = "";
+		}
+		
+		result[0] = search_option;
+		result[1] = search_data;
+		result[2] = search_date_start;
+		result[3] = search_date_end;
+		result[4] = search_date_check;
+		
+		return result;
+	}
+}
---
+++ src/controller/memo/MemoController.java
@@ -0,0 +1,108 @@
+package controller.memo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.Util;
+import model.memo.dao.MemoDAO;
+import model.memo.dto.MemoDTO;
+
+/**
+ * Servlet implementation class MemoController
+ */
+@WebServlet("/memo_servlet/*")
+public class MemoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+	
+	protected void doProc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String path = request.getContextPath();
+		String url = request.getRequestURL().toString();
+		String page = "/main/main.jsp";
+		Util util = new Util();
+		
+		if (url.indexOf("write.do") != -1) {
+			request.setAttribute("menu_gubun", "memo_write");
+			RequestDispatcher rd = request.getRequestDispatcher(page);
+			rd.forward(request, response);
+			
+			
+		} else if (url.indexOf("writeProc.do") != -1) {
+			String writer = request.getParameter("writer");
+			String content = request.getParameter("content");
+			
+			MemoDTO dto = new MemoDTO();
+			dto.setWriter(writer);
+			dto.setContent(content);
+			
+			MemoDAO dao = new MemoDAO();
+			String temp;
+			dao.setInsert(dto);
+			
+			
+		} else if (url.indexOf("list.do") != -1) {
+			MemoDAO dao = new MemoDAO();
+			
+			String pageNum_ = request.getParameter("page");
+
+			// validation. charAt을 이용하거나 정규표현식을 이용해 숫자만 남기고 걸러야함
+			int pageNum = util.numberCheck(pageNum_, 1);
+			
+			// paging
+			final int ONE_PAGE_ROWS = 8;
+			final int MAX_PAGING_WIDTH = 10;
+			int allRowsCount = dao.getAllRowsCount();
+			int maxPagesCount = (int) Math.ceil((double) allRowsCount / ONE_PAGE_ROWS);
+			int tableRowNum = allRowsCount - (pageNum - 1) * ONE_PAGE_ROWS;
+			int pagingLoopNum = (int) Math.ceil((double)pageNum / MAX_PAGING_WIDTH) - 1;
+			int pagingStartNum = pagingLoopNum * MAX_PAGING_WIDTH + 1;
+			int pagingEndNum = pagingStartNum + MAX_PAGING_WIDTH - 1;
+			if (pagingEndNum > maxPagesCount) {
+				pagingEndNum = maxPagesCount;
+			}
+			int endNum = pageNum * ONE_PAGE_ROWS;
+			int startNum = endNum - ONE_PAGE_ROWS + 1;
+			
+			ArrayList<MemoDTO> list = dao.getPagingList(startNum, endNum);
+			
+			request.setAttribute("list", list);
+			request.setAttribute("allRowsCount", allRowsCount);
+			request.setAttribute("pageNum", pageNum);
+			request.setAttribute("tableRowNum", tableRowNum);
+			request.setAttribute("maxPagesCount", maxPagesCount);
+			request.setAttribute("pagingStartNum", pagingStartNum);
+			request.setAttribute("pagingEndNum", pagingEndNum);
+			
+			page = "/memo/list.jsp";
+			
+			RequestDispatcher rd = request.getRequestDispatcher(page);
+			rd.forward(request, response);
+			
+			
+		} else if (url.indexOf("deleteInfo.do") != -1) {
+			String no_ = request.getParameter("no");
+			int no = Integer.parseInt(no_);
+			
+			MemoDAO dao = new MemoDAO();
+			MemoDTO dto = new MemoDTO();
+			dto.setNo(no);
+			dao.setDelete(dto);
+		}
+	}
+
+}
